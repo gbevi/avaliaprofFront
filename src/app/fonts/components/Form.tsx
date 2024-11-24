fc73458fa72216834dc8c8b9b522e5c5a36f938c
audittmega <ovlogsebatata.1534vabata@gmail.com>
2024-11-24 15:35:45 -0300

    front ajustado

--- src/app/fonts/components/Form.tsx
+++ src/app/fonts/components/Form.tsx
@@ -3,22 +3,22 @@ import * as React from 'react';
 export default function Form() {
     return (
         <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-200'>
-            <h1 className="text-5xl font semibold">Welcome Back</h1>
-            <p className="font-medium text-lg text-gray-500 mt-4">Welcome Back! Please enter your details.</p>
+            <h1 className="text-5xl font semibold">Avaliação de Professores</h1>
+            <p className="font-medium text-lg text-gray-500 mt-4">Cadastre seus Dados:</p>
             <div className="mt-8">
                 <div>
                     <label className="text-lg font-medium">Email</label>
                     <input
                         className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
-                        placeholder="Enter your email"
+                        placeholder="Digite seu Email"
                         type="email"
                     />
                 </div>
                 <div>
-                    <label className="text-lg font-medium">Password</label>
+                    <label className="text-lg font-medium">Senha</label>
                     <input
                         className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
-                        placeholder="Enter your password"
+                        placeholder="Digite sua senha"
                         type="password"
                     />
                 </div>
@@ -28,16 +28,16 @@ export default function Form() {
                             type="checkbox"
                             id='remember'
                         />
-                        <label className="ml-2 font-medium text-base" htmlFor="remember">Remember for 30 days</label>
+                        <label className="ml-2 font-medium text-base" htmlFor="remember">Lembre de mim</label>
                     </div>
-                    <button className="font-medium text-base text-green-500">Forgot password</button>
+                    <button className="font-medium text-base text-green-500">Esqueci a Senha</button>
                 </div>
                 <div className="mt-8 flex flex-col gap-y-4">
-                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-green-500 text-white text-lg font-bold">Sign in</button>
+                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-green-500 text-white text-lg font-bold">Entrar</button>
                 </div>
                 <div className="mt-8 flex justify-center items-center">
-                    <p className="font-medium text-base">Don&apos;t have an account?</p>
-                    <button className="text-green-500 text-base font-medium ml-2">Sign up</button>
+                    <p className="font-medium text-base">Não possui conta?</p>
+                    <button className="text-green-500 text-base font-medium ml-2">Criar Conta</button>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,14 @@ export default function Home() {
         {/* Formulário na metade esquerda */}
         <Form />
       </div>
-      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-green-200">
-        {/* Círculo com gradiente personalizado */}
-        <div
-          className="w-60 h-60 rounded-full animate-spin duration-9999"
-          style={{
-            background: "linear-gradient(to top right, rgb(144, 238, 144), rgb(0, 128, 0), rgb(0, 0, 255))",
-          }}
-        ></div>
 
-        {/* Blur na metade inferior direita com cor personalizada */}
-        <div
-          className="absolute bottom-0 right-0 w-full h-1/2 backdrop-blur-lg"
-          style={{
-            backgroundColor: "rgba(93, 89, 86, 0.2)",
-          }}
-        ></div>
+      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-green-200">
+        {/* Adicionando a imagem */}
+        <img
+          src="/images/unblogo.png" // Caminho relativo para a imagem na pasta public
+          alt="Logo da Universidade"
+          className="w-1/2 h-auto" // Ajuste o tamanho conforme necessário
+        />
       </div>
     </div>
   );
